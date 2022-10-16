a5c0f656f9a1c0ff1d84bf53eefaf33ded9b5a23
George Okoli <cypriangeorge18@gmail.com>
2022-10-16 12:45:52 -0700

    Jesus

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,15 @@
 
 /**
  * main- Entry point
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char low;
-	for (low = a; low <= z; low++)
-		putchar(low) ;
-		putchar('\n') ;
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n')
 	return (0);
 }
