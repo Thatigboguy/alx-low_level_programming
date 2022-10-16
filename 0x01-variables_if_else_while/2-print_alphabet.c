5693c9a3d43685b60de9c52e3a6b8197e28cab25
George Okoli <cypriangeorge18@gmail.com>
2022-10-16 14:05:13 -0700

    l

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,3 @@
-/*
- * File: 2-print_alphabet.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
@@ -14,7 +9,7 @@
 int main(void)
 {
 	char letter;
-	for (letter = 'a'; letter <= 'z'; low++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 		putchar('\n');
 	return (0);
