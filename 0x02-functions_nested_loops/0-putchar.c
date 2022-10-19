21eec05f49689377878568f90ff949e159f6b05b
George Okoli <cypriangeorge18@gmail.com>
2022-10-18 17:14:48 -0700

    jesus

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - prints _putchar, fllowd by a new lines
+ * Return: Alwys 0 (Success)
  */
-
 int main(void)
 {
-	char *sh = "Holberton";
-	while (*sh)
+	char abe[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
 	{
-		_putchar(*sh);
-		sh++;
+		_putchar(abe[c]);
 	}
 	_putchar('\n');
 	return (0);
