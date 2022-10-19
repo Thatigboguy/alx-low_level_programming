7cd6c113759409f044ce07a4d80ea0b209f9af70
George Okoli <cypriangeorge18@gmail.com>
2022-10-19 04:11:02 -0700

    Jesus

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
  */
 #include "main.h"
 /**
- * main-print putchar followed by a new line
+ * main - print putchar followed by a new line
  * Return :always 0
  */
 int main(void)
@@ -17,5 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n);
+	
 	return(0) ;
 }
