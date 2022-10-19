ee96c0e8987fbe67d0c8ee8d0f8e62587ed99dbd
George Okoli <cypriangeorge18@gmail.com>
2022-10-19 05:19:51 -0700

    j

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - check the code for ALX School students.
- *
- * Return: Always 0.
+ * print_alphabet - Entry point
+ * Return:0
  */
+
 void print_alphabet(void)
 {
-	char alp = 'a';
-	
-	while (alp <= 'z')
+	char c = 'a';
+
+	while (c <= 'z')
 	{
-		_putchar (alp);
-		alp++;
+		_putchar(c);
+		c++;
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
